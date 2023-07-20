ae7f1938b814a9dc07e6422d3a674096c90a35bb
landon rohatensky <landonroha@gmail.com>
2023-07-20 06:32:05 -0700

    fix top bar, add bottom bar, add xml layout

---
+++ app/src/main/java/com/example/firstapp/DetailActivity.kt
@@ -0,0 +1,97 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.firstapp.ui.theme.FirstAppTheme
+
+@Composable
+fun DetailActivity(navController: NavHostController, detailedInfo: Pair<WorkoutItems, TrainerModel?>) {
+    FirstAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column() {
+                AsyncImage(
+                    model = ImageRequest.Builder(context = LocalContext.current)
+                        .data(detailedInfo.first.previewImgUrl)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = detailedInfo.first.desc,
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = detailedInfo.first.desc?: "",
+                    modifier = Modifier.padding(16.dp)
+                )
+                detailedInfo.second?.let { trainer ->
+                    trainerCard(trainer, modifier = Modifier.padding(16.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun trainerCard(trainer: TrainerModel, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Row() {
+            val trainerName =
+                trainer.firstName + " " + trainer.lastName
+            Text(
+                text = trainerName,
+                modifier = Modifier.padding(16.dp)
+            )
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(trainer.photoUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = trainerName,
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    FirstAppTheme {
+        Greeting("Android")
+    }
+}
--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.firstapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,76 +14,184 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.rounded.AddCircle
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.firstapp.ui.theme.FirstAppTheme
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import kotlinx.coroutines.CoroutineName
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.lang.Exception
-import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.Date
 
+
 class MainActivity : ComponentActivity() {
+    data class BottomNavItem(
+        val name: String,
+        val route: String,
+        val icon: ImageVector,
+    )
+    val bottomNavItems = listOf(
+        BottomNavItem(
+            name = "Compose",
+            route = "column",
+            icon = Icons.Rounded.Home,
+        ),
+        BottomNavItem(
+            name = "XML",
+            route = "xmlColumn",
+            icon = Icons.Rounded.AddCircle,
+        )
+    )
+
+    @Composable
+    fun currentRoute(navController: NavHostController): String? {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        return navBackStackEntry?.destination?.route
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstAppTheme {
-                val padding = 16.dp
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    val viewModel = remember { ItemViewModel() }
-                    MyColumn(viewModel)
+            val navController = rememberNavController()
+            val backStackEntry = navController.currentBackStackEntryAsState()
+            val topBarState = rememberSaveable { (mutableStateOf(true)) }
+            when(currentRoute(navController = navController)) {
+                "column" -> {
+                    topBarState.value = false
+                }
+                "detail/{itemId}" -> {
+                    topBarState.value = true
+                }
+                "xmlColumn" -> {
+                    topBarState.value = false
+                }
+            }
+            Scaffold(
+                bottomBar = {
+                    androidx.compose.material3.NavigationBar(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                    ) {
+                        bottomNavItems.forEach { item ->
+                            val selected = item.route == backStackEntry.value?.destination?.route
+
+                            NavigationBarItem(
+                                selected = selected,
+                                onClick = { navController.navigate(item.route) },
+                                label = {
+                                    Text(
+                                        text = item.name,
+                                        fontWeight = FontWeight.SemiBold,
+                                    )
+                                },
+                                icon = {
+                                    Icon(
+                                        imageVector = item.icon,
+                                        contentDescription = "${item.name} Icon",
+                                    )
+                                }
+                            )
+                        }
+                    }
+                },
+                topBar = {
+                    if (topBarState.value) {
+                        TopAppBar(
+                            title = {
+                                Text("My App")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { navController.navigateUp() }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.ArrowBack,
+                                        contentDescription = "Back"
+                                    )
+                                }
+                            },
+                            colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = Color.Cyan)
+                        )
+                    }
+                }) { contentPadding ->
+                FirstAppTheme {
+                    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                        val viewModel = remember { ItemViewModel() }
+                        NavHost(
+                            navController = navController,
+                            startDestination = "column"
+                        ) {
+                            composable("column") {
+                                MyColumn(viewModel, navController, modifier = Modifier.padding(contentPadding))
+                            }
+                            composable("detail/{itemId}") { backStackEntry ->
+                                val itemId = backStackEntry.arguments?.getString("itemId")
+                                viewModel.result.first { it.first.id.toString() == itemId }?.let {
+                                    DetailActivity(navController, it)
+                                }
+                            }
+                            composable("xmlColumn") {
+                                AndroidView(
+                                    modifier = Modifier.fillMaxSize(),
+                                    factory = { context ->
+                                        layoutInflater.inflate(R.layout.layout, null, true)
+                                    }
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -153,7 +262,7 @@ fun LazyListState.OnBottomReached(
 }
 
 @Composable
-fun MyColumn(viewModel: ItemViewModel = ItemViewModel() ) {
+fun MyColumn(viewModel: ItemViewModel = ItemViewModel(), navController: NavHostController, modifier: Modifier = Modifier) {
     val padding = 16.dp
     val textState = remember { mutableStateOf(TextFieldValue("")) }
     val listState = rememberLazyListState()
@@ -164,7 +273,9 @@ fun MyColumn(viewModel: ItemViewModel = ItemViewModel() ) {
             SearchBar(textState)
             LazyColumn(state = listState) {
                 items(GetFilteredList(viewModel.result, textState)) {
-                    WorkoutCard(modifier = Modifier, workoutItem = it)
+                    WorkoutCard(modifier = Modifier.clickable {
+                        navController.navigate("detail/${it.first.id}")
+                    }, workoutItem = it)
                 }
                 if (viewModel.isLoading) { // Assuming you have a flag to indicate loading state
                     item {
@@ -211,7 +322,10 @@ fun WorkoutCard(modifier: Modifier = Modifier, workoutItem: Pair<WorkoutItems, T
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
         Row() {
-            WorkoutCardInfo(modifier.weight(1f).padding(8.dp), workoutItem)
+            WorkoutCardInfo(
+                modifier
+                    .weight(1f)
+                    .padding(8.dp), workoutItem)
             Box(modifier = Modifier
                 .background(Color.DarkGray)
                 .width(148.dp)
@@ -267,6 +381,7 @@ private fun WorkoutCardInfo(
 @Composable
 fun GreetingPreview() {
     FirstAppTheme {
-        MyColumn()
+        val navController = rememberNavController()
+        MyColumn(navController = navController)
     }
 }

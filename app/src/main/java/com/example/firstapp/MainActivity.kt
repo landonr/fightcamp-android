74ecc10c4796875fdf8e3ee04ff84d0080d31ddb
landon rohatensky <landonroha@gmail.com>
2023-07-31 06:15:39 -0700

    cleanup navcontroller

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -19,22 +20,45 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         val layout = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar_layout)
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
+        setupBottonNavigationView(navController, layout)?.run {
+            setupDestinationChangedListener(navController, layout, this)
+        }
+    }
+
+    private fun setupBottonNavigationView(
+        navController: NavController,
+        layout: CollapsingToolbarLayout
+    ): BottomNavigationView? {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView.setupWithNavController(navController)
         layout.setupWithNavController(toolbar, navController, appBarConfiguration)
+        return bottomNavigationView
+    }
+
+    private fun setupDestinationChangedListener(
+        navController: NavController,
+        layout: CollapsingToolbarLayout,
+        bottomNavigationView: BottomNavigationView
+    ) {
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            if(destination.id == R.id.workoutDetailFragment) {
-                layout.visibility = View.VISIBLE
-                bottomNavigationView.visibility = View.GONE
-            } else {
-                layout.visibility = View.GONE
-                bottomNavigationView.visibility = View.VISIBLE
-            }
+            val layoutConfiguration = currentLayoutConfiguration(destination.id)
+            layout.visibility = layoutConfiguration.layoutVisible
+            bottomNavigationView.visibility = layoutConfiguration.bottonNavigationViewVisible
+        }
+    }
+
+    class LayoutConfiguration(val layoutVisible: Int, val bottonNavigationViewVisible: Int) {}
+    fun currentLayoutConfiguration(destination: Int): LayoutConfiguration {
+        return when(destination) {
+            R.id.workoutDetailFragment -> LayoutConfiguration(View.VISIBLE, View.GONE)
+            R.id.workoutFragment -> LayoutConfiguration(View.GONE, View.VISIBLE)
+            R.id.componentEmbedFragment -> LayoutConfiguration(View.GONE, View.VISIBLE)
+            else -> LayoutConfiguration(View.VISIBLE, View.GONE)
         }
     }
 }
